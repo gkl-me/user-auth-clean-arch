b89fc3ff705c06b4c079f3e6129dcba5724a27a5
Gokul G R <gklgrme@gmail.com>
2024-11-05 22:20:47 +0530

    final setp

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("hello world")
+import express from "express";
+import cookieParser from 'cookie-parser'
+import { container } from "./di/container";
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+app.post('/api/register',(req,res) => container.authController.register(req,res))
+app.post('/api/login',(req,res) => container.authController.login(req,res))
+
+
+
+app.listen(3000,()=>{
+    console.log('server running')
+})
